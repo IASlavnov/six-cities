8519efa66ed65d34a21e550e019d2fab2b08b370
ISlavnov <iaslavnov@gmail.com>
2021-10-19 00:25:48 +0300

    added screen components

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,13 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainScreen from '../main-screen/main-screen';
+
+import { Offer } from '../../types/offer';
+
+type AppProps = {
+  offers: Offer[],
+};
+
+function App({ offers }: AppProps): JSX.Element {
+  return <MainScreen offers={offers} />;
 }
 
 export default App;
